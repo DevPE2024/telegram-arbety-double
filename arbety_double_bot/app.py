9e81f55627fc81d7fe6ccdc899bc249fbca3d711
riguima <richard.alexsander.guima@gmail.com>
2023-06-30 21:21:51 -0300

    add add_strategy function in app]

--- arbety_double_bot/app.py
+++ arbety_double_bot/app.py
@@ -19,6 +19,7 @@ def create_app() -> Client:
         await message.reply(
             (
                 '/login - Para cadastrar o login da plataforma arbety\n'
+                '/adicionar_estrategia - Para adicionar uma estratégia, junto com o valor de aposta'
             )
         )
 
@@ -29,9 +30,32 @@ def create_app() -> Client:
         driver = create_driver()
         make_login(driver, email.text, password.text)
         if is_logged(driver):
+            create_user(email.text, password.text)
             await client.send_message(message.chat.id, 'Login cadastrado')
         else:
             await client.send_message(message.chat.id, 'Login inválido')
         driver.quit()
 
+    @app.on_message(filters.command('adicionar_estrategia'))
+    async def add_strategy(client: Client, message: Message) -> None:
+        strategy = await client.ask(
+            message.chat.id,
+            (
+                'Digite sua estratégia utilizando r (red), g (green) e w '
+                '(white), exemplo: r - r - g'
+            ),
+        )
+        strategy_regex = re.compile()
+        value = await client.ask(
+            'Digite o valor para a aposta, exemplo: 50 ou 50,00'
+        )
+        try:
+            float(value.text.replace(',', '.'))
+            if strategy_regex.findall(strategy.text):
+                await strategy.reply('Estratégia adicionada')
+                create_strategy(strategy.text)
+            else:
+                await strategy.reply('Estratégia definida incorretamente')
+        except ValueError:
+            await value.reply('Digite apenas números para o valor da aposta')
     return app
